c99d575bc859c8574155fa91435821a41bde9ab4
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-12-17 13:10:33 +0300

    level 206 was solved.

---
+++ level206.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+
+/*
+Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
+where each “_” is a single digit.
+*/
+
+/*
+I've solved this problem and published solution on "Thread 206."
+Check my name "Ustrif".
+*/
